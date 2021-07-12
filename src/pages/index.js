7e66a30d2a07089922f70f8385fca6fa6bdc84c4
justintd <justintdinh1@gmail.com>
2021-07-12 11:55:56 -0700

    Adjusted folder hierarchy and imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import NavBar from "../components/NavBar/NavBar"
-import Hero from "../components/Hero"
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import Hero from '../components/Hero/Hero';
 
 export default function Home() {
   return (
@@ -8,5 +8,5 @@ export default function Home() {
       <NavBar />
       <Hero />
     </div>
-  )
+  );
 }
