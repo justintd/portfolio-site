b72398a7f686dbbfc3ef6ce23b9f3d9ec58fdd74
justintd <justintdinh1@gmail.com>
2021-07-13 11:18:21 -0700

    Adjusts text and remove react reveal config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import config from 'react-reveal/globals';
 import NavBar from '../components/NavBar/NavBar';
 import Hero from '../components/Hero/Hero';
 import AboutMe from '../components/AboutMe/AboutMe';
 import Work from '../components/Work/Work';
 import Footer from '../components/Footer/Footer';
 import Contact from '../components/Contact/Contact';
-
-config({ ssrFadeout: true });
 
 export default function Home() {
   return (
