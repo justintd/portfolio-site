0f30f52b5bf69a52801809cbb247211cd5afbf51
justintd <justintdinh1@gmail.com>
2021-07-12 17:49:27 -0700

    Adds Contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from '../components/Hero/Hero';
 import AboutMe from '../components/AboutMe/AboutMe';
 import Work from '../components/Work/Work';
 import Footer from '../components/Footer/Footer';
+import Contact from '../components/Contact/Contact';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <AboutMe />
       <Work />
+      <Contact />
       <Footer />
     </div>
   );
