5c7ee0bcb4ec38d4edfcb5714e7d08d5ab1fc2ba
justintd <justintdinh1@gmail.com>
2021-07-12 20:04:15 -0700

    Adjusts styling on 404 page. Adds Fade to all section components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import Fade from 'react-reveal';
 import GithubIcon from '../icons/Github';
 import LinkedInIcon from '../icons/LinkedIn';
 import MailIcon from '../icons/Mail';
 import './Footer.css';
 
 const Footer = () => (
-  <footer className="container mx-auto px-8">
-    <div className="flex justify-between items-center h-24">
-      <div className="flex flex-row text-md text-white font-sans tracking-wide items-center justify-center">
-        <a href="https://jdinh.dev" className="mr-2">
-          <p>Justin Dinh © 2021</p>
-        </a>
+  <Fade bottom>
+    <footer className="container mx-auto px-8">
+      <div className="flex justify-between items-center h-24">
+        <div className="flex flex-row text-md text-white font-sans tracking-wide items-center justify-center">
+          <a href="https://jdinh.dev" className="mr-2">
+            <p>Justin Dinh © 2021</p>
+          </a>
+        </div>
+        <div className="flex flex-row items-center">
+          <a className="mail-icon mr-5" href="mailto:justintdinh1@gmail.com">
+            <MailIcon />
+          </a>
+          <a className="github-icon mr-5" href="https://github.com/justintd/portfolio-site">
+            <GithubIcon />
+          </a>
+          <a className="linkedin-icon" href="https://github.com/justintd/portfolio-site">
+            <LinkedInIcon />
+          </a>
+        </div>
       </div>
-      <div className="flex flex-row items-center">
-        <a className="mail-icon mr-5" href="mailto:justintdinh1@gmail.com">
-          <MailIcon />
-        </a>
-        <a className="github-icon mr-5" href="https://github.com/justintd/portfolio-site">
-          <GithubIcon />
-        </a>
-        <a className="linkedin-icon" href="https://github.com/justintd/portfolio-site">
-          <LinkedInIcon />
-        </a>
-      </div>
-    </div>
-  </footer>
+    </footer>
+  </Fade>
 );
 
 export default Footer;
--- src/pages/404.js
+++ src/pages/404.js
@@ -7,17 +7,19 @@ export default function NotFound() {
     <div>
       <NavBar />
       <section className="container mx-auto px-8 lg:px-64 py-24 lg:py-40 h-screen">
-        <div className="flex flex-col justify-center align-center">
-          <h1 className="text-4xl mb-5">Yikes!</h1>
-          <h3 className="text-2xl mb-5">Unfortunately, nothing&apos;s here.</h3>
-          <p className="text-lg mb-5">
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <h1 className="text-6xl text-melon font-sans tracking-wide font-bold mb-3">Yikes!</h1>
+          <h3 className="text-2xl text-white font-sans tracking-wide mb-3">
+            Unfortunately, nothing&apos;s here.
+          </h3>
+          <p className="text-lg text-white font-sans tracking-wide mb-5">
             Maybe there&apos;s a typo in the URL or the page no longer exists?
           </p>
-          <a href="/#">
+          <a href="/">
             <button
               className="py-1 px-2 sm:py-2 lg:px-5 lg:text-md text-xs sm:text-sm rounded-sm border focus:outline-none font-mono text-melon tracking-wide hover:bg-melon hover:bg-opacity-25 transition duration-300"
               type="button">
-              Go Back home
+              Go Back Home
             </button>
           </a>
         </div>
